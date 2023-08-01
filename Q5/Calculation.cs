3fd1e910158f6514464c24618371a070ecec397f
Noman Ahmed <nomanpb234@gmail.com>
2023-08-01 12:12:55 +0500

    Add project files.

---
+++ Q5/Calculation.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q5
+{
+    internal class Calculation
+    {
+        private double a,b;
+
+        public double _a
+        {
+            set { a = value; }
+            get { return a; }
+        } 
+        
+        public double _b
+        {
+            set { b = value; }
+            get { return b; }
+        }
+        public void Add()
+        {
+            Console.WriteLine("Sum = {0}",a+b);
+        }
+
+        public void Subtract()
+        {
+            Console.WriteLine("Difference = {0}", a - b);
+        }
+
+        public void Divide()
+        {
+            Console.WriteLine("Quotient = {0}", a / b);
+        }
+
+        public void Multiply()
+        {
+            Console.WriteLine("Product = {0}", a * b);
+        }
+    }
+}
---
+++ Q5/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Write code which creates a class Calculation and declare methods like Add() and Subtract(). Take two private variables a abnd b and take user input for these variables that would later be passed to the appropriate method for calculations*/
+
+namespace Q5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1, num2;
+            Console.WriteLine("Enter Number 1: ");
+            num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter Number 2: ");
+            num2 = Convert.ToDouble(Console.ReadLine());
+            Calculation cal = new Calculation();
+            cal._a = num1;
+            cal._b = num2;
+            cal.Add();
+            cal.Subtract();
+            cal.Divide();
+            cal.Multiply();
+        }
+    }
+}
